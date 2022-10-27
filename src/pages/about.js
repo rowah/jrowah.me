e9c45037d8fb66ae33c1efde175c1ff19a53a842
Rowa <jrowah@gmail.com>
2022-10-27 16:53:40 +0300

    updates about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,12 +4,22 @@ import Layout from "../components/layout";
 const AboutPage = () => {
   return (
     <main>
-      <Layout pageTitle="About Me">
+      <Layout pageTitle="James Rowa">
+        <h1>Software Developer</h1>
+        <hr />
         <p>
-          Hello, my name is James Rowa. I am a self and community taugh software
-          developer from Nairobi, Kenya.
+          Hello, my name is James Rowa.
+          <br /> I am a self and community taugh software developer from
+          Nairobi, Kenya. <br /> My interests include AI, Data Science, Object
+          Oriented Programming.
+          <br /> I am also a professional biochemist, and an educator.
         </p>
-        <h2>Stack</h2>
+        <p>
+          I am also a technical writer on Medium. <br />
+          Check out my Blog. <br />
+          Read about My Journey into Tech!
+        </p>
+        <h2>Skill Set</h2>
         <p>I am a MERN stack developer with proficiency in the following:</p>
         <ul>
           <li>JavaScript</li>
@@ -20,6 +30,7 @@ const AboutPage = () => {
           <li>CSS</li>
           <li>Tailwind</li>
         </ul>
+        <hr />
       </Layout>
     </main>
   );
