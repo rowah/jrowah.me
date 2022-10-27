ddbeedc323da8134b56a259f0565e75288600910
Rowa <jrowah@gmail.com>
2022-10-27 03:34:45 +0300

    adds layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,26 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
   return (
     <main>
-      <h1>About Me</h1>
-      <p>
-        Hello, my name is James Rowa. I am a self and community taugh software
-        developer from Nairobi, Kenya.
-      </p>
-      <h2>Stack</h2>
-      <p>I am a MERN stack developer with proficiency in the following:</p>
-      <ul>
-        <li>JavaScript</li>
-        <li>ReactJS</li>
-        <li>MongoDB</li>
-        <li>NodeJS</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Tailwind</li>
-      </ul>
-      <Link to="/">Back to Home</Link>
+      <Layout pageTitle="About Me">
+        <p>
+          Hello, my name is James Rowa. I am a self and community taugh software
+          developer from Nairobi, Kenya.
+        </p>
+        <h2>Stack</h2>
+        <p>I am a MERN stack developer with proficiency in the following:</p>
+        <ul>
+          <li>JavaScript</li>
+          <li>ReactJS</li>
+          <li>MongoDB</li>
+          <li>NodeJS</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Tailwind</li>
+        </ul>
+      </Layout>
     </main>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <main>
-      <h1>Hi, I'm James Rowa</h1>
-      <Link to="/about">About</Link>
-      <p>This is my Portfolio Website</p>
+      <Layout pageTitle="Home Page">
+        <p>This is my Portfolio Website</p>
+      </Layout>
     </main>
   );
 };
