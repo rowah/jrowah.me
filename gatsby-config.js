5e5092e1d3e565ea10322a03d233caa28553e1f0
Rowa <jrowah@gmail.com>
2022-10-31 21:33:20 +0300

    adds image and skills

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,4 +4,10 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
-}
+};
+module.exports = {
+  siteMetadata: {
+    title: "My First Gatsby Site",
+  },
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { jrowah, aeso } from "./about.module.css";
 
 const AboutPage = () => {
   return (
@@ -7,29 +9,27 @@ const AboutPage = () => {
       <Layout pageTitle="James Rowa">
         <h1>Software Developer</h1>
         <hr />
-        <p>
-          Hello, my name is James Rowa.
-          <br /> I am a self and community taugh software developer from
-          Nairobi, Kenya. <br /> My interests include AI, Data Science, Object
-          Oriented Programming.
-          <br /> I am also a professional biochemist, and an educator.
-        </p>
-        <p>
-          I am also a technical writer on Medium. <br />
-          Check out my Blog. <br />
-          Read about My Journey into Tech!
-        </p>
-        <h2>Skill Set</h2>
+        <section>
+          <div className={aeso}>
+            Self and community taugh software developer <br /> from Nairobi,
+            Kenya. <br /> My interests include AI, Data Science, <br /> and
+            Object Oriented Programming.
+            <br /> I am also a professional biochemist, and an educator. <br />I
+            am also a technical writer on Medium. <br />
+            Check out my Blog. <br />
+            Read about My Journey into Tech!
+          </div>
+          <StaticImage
+            alt="Jrowah's picture"
+            src="../images/devjay.jpeg"
+            className={jrowah}
+          />
+        </section>
+        <h2>Skill Set ?</h2>
         <p>I am a MERN stack developer with proficiency in the following:</p>
-        <ul>
-          <li>JavaScript</li>
-          <li>ReactJS</li>
-          <li>MongoDB</li>
-          <li>NodeJS</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Tailwind</li>
-        </ul>
+        <span>JavaScript ~ </span> <span>ReactJS ~ </span>{" "}
+        <span>MongoDB ~ </span> <span>NodeJS ~ </span> <span>HTML ~ </span>{" "}
+        <span>CSS ~ </span> <span>Tailwind</span>
         <hr />
       </Layout>
     </main>
