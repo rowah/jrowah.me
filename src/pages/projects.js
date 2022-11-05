5608815163fa9a8d8b9110fe6af1b769b2ac5685
Rowa <jrowah@gmail.com>
2022-11-05 03:22:55 +0300

    links first project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,25 @@ import * as React from "react";
 import Footer from "../components/footer";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { container } from "../components/layout.module.css";
 
 const ProjectsPage = () => {
   return (
     <main>
       <Layout pageTitle="Projects">
-        <p>These are some of my Projects</p>
+        <div className={container}>
+          <a
+            href="https://product-expiry-tracker.cyclic.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            PTracker
+          </a>
+          <p>
+            PTracker helps minimize westage through disposal of expired goods by
+            making it easy to track the expiry dates of products.
+          </p>
+        </div>
       </Layout>
       <Footer>
         <ul>
