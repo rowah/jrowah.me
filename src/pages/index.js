060356f95f392fd0b3013c30e002d7813f640578
Rowa <jrowah@gmail.com>
2022-11-16 03:58:25 +0300

    colors the skills compo

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,23 +77,34 @@ const AboutPage = () => {
           </p>
           <span>
             <strong>
-              <span className={yellow}>Java</span>
+              <span class="text-yellow-500" className={yellow}>
+                Java
+              </span>
               <span>Script</span>
             </strong>{" "}
             ~{" "}
           </span>
           &nbsp;
           <span>
-            <strong className={blue}>ReactJS</strong> ~{" "}
+            <strong class="text-blue-600/100" className={blue}>
+              ReactJS
+            </strong>{" "}
+            ~{" "}
           </span>
           &nbsp;
           <span>
-            <strong className={green}>MongoDB</strong> ~
+            <strong class="text-green-700" className={green}>
+              MongoDB
+            </strong>{" "}
+            ~
           </span>
           &nbsp;
           <span>
             <strong>
-              N<span className={green}>o</span>
+              N
+              <span class="text-green-700" className={green}>
+                o
+              </span>
               deJS
             </strong>{" "}
             ~
@@ -102,25 +113,39 @@ const AboutPage = () => {
           <span>
             <strong>
               <span>H</span>
-              <span className={tee}>T</span>
-              <span className={em}>M</span>
-              <span className={el}>L</span>
+              <span class="text-orange-600" className={tee}>
+                T
+              </span>
+              <span class="text-red-600" className={em}>
+                M
+              </span>
+              <span class="text-gray-400" className={el}>
+                L
+              </span>
             </strong>{" "}
             ~
           </span>
           &nbsp;
           <span>
-            <strong className={css}>CSS</strong> ~
+            <strong class="text-blue-800" className={css}>
+              CSS
+            </strong>{" "}
+            ~
           </span>
           &nbsp;
           <span>
             {" "}
-            <strong className={blue}>Tailwind</strong> ~
+            <strong className={blue} class="text-blue-600">
+              Tailwind
+            </strong>{" "}
+            ~
           </span>
           &nbsp;
           <span>
             {" "}
-            <strong className={purple}>Elixir</strong>
+            <strong class="text-purple-700" className={purple}>
+              Elixir
+            </strong>
             <span>(currently learning)</span>
           </span>
           <h2 class="text-2xl mt-6 mb-4">Fun Time ?</h2>
