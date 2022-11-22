a4e799afee4ae51ba33bb623b8e5e166d3605d48
Rowa <jrowah@gmail.com>
2022-11-22 12:39:53 +0300

    updates blogs

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -3,14 +3,50 @@ import { graphql } from "gatsby";
 import Layout from "../../components/layout";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Seo from "../../components/seo";
+import { blogImg } from "../about.module.css";
 
 const BlogPost = ({ data, children }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>Posted: {data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={image} />
-      {children}
+      <GatsbyImage image={image} className={blogImg} />
+      <section class="bg-slate-100 w-5/6 shadow-2xl p-8 font-serif leading-relaxed text-lg">
+        {children}
+        <h3 align="center">Connect With Me</h3>
+        <p class="flex justify-center">
+          <span class="px-4">
+            <a href="https://twitter.com/jrowah" target="blank">
+              <img
+                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+                alt="jrowah"
+                height="20"
+                width="30"
+              />
+            </a>
+          </span>
+          <span class="px-4">
+            <a href="https://linkedin.com/in/james-rowa" target="blank">
+              <img
+                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                alt="james rowa"
+                height="20"
+                width="30"
+              />
+            </a>
+          </span>
+          <span class="px-4">
+            <a href="https://github.com/rowah" target="blank">
+              <img
+                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg"
+                alt="james rowa"
+                height="20"
+                width="30"
+              />
+            </a>
+          </span>
+        </p>
+      </section>
     </Layout>
   );
 };
