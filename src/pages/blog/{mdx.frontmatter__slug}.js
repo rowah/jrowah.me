6b820d4e9c43a797f0b8733bc0774ba85845c585
Rowa <jrowah@gmail.com>
2023-03-12 22:29:26 +0300

    modifies each blog display

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -10,60 +10,62 @@ const BlogPost = ({ data, children }) => {
   const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <section class="bg-slate-100 shadow-2xl p-8 font-serif leading-relaxed text-lg w-6/6 md:w-5/6 lg:w-5/6 max-w-4xl m-auto ml-20">
-        <p>Posted On: {data.mdx.frontmatter.date}</p>
-        <GatsbyImage image={image} className={blogImg} />
-        <p class="text-right mr-20">
-          Photo Credit: {data.mdx.frontmatter.hero_image_credit_text}
-        </p>
-        <br />
-        {children}
-        <h3 align="right" class="mr-4">
-          Connect With Me
-        </h3>
-        <p class="flex justify-end mt-4">
-          <span class="px-4">
-            <a href="https://twitter.com/jrowah" target="blank">
-              <img
-                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
-                alt="jrowah"
-                height="20"
-                width="30"
-              />
-            </a>
-          </span>
-          <span class="px-4">
-            <a href="https://instagram.com/jrowah" target="blank">
-              <img
-                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg"
-                alt="jrowah"
-                height="20"
-                width="30"
-              />
-            </a>
-          </span>
-          <span class="px-4">
-            <a href="https://linkedin.com/in/james-rowa" target="blank">
-              <img
-                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
-                alt="james rowa"
-                height="20"
-                width="30"
-              />
-            </a>
-          </span>
-          <span class="px-4">
-            <a href="https://github.com/rowah" target="blank">
-              <img
-                src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg"
-                alt="james rowa"
-                height="20"
-                width="30"
-              />
-            </a>
-          </span>
-        </p>
-      </section>
+      <div class="max-w-4xl bg-slate-200">
+        <section class="bg-slate-100 shadow-2xl p-8 font-serif leading-relaxed text-lg w-6/6 md:w-5/6 lg:w-5/6 max-w-4xl m-auto ml-20">
+          <p>Posted On: {data.mdx.frontmatter.date}</p>
+          <GatsbyImage image={image} className={blogImg} />
+          <p class="text-right mr-20">
+            Photo Credit: {data.mdx.frontmatter.hero_image_credit_text}
+          </p>
+          <br />
+          {children}
+          <h3 align="right" class="mr-4">
+            Connect With Me
+          </h3>
+          <p class="flex justify-end mt-4">
+            <span class="px-4">
+              <a href="https://twitter.com/jrowah" target="blank">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+                  alt="jrowah"
+                  height="20"
+                  width="30"
+                />
+              </a>
+            </span>
+            <span class="px-4">
+              <a href="https://instagram.com/jrowah" target="blank">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg"
+                  alt="jrowah"
+                  height="20"
+                  width="30"
+                />
+              </a>
+            </span>
+            <span class="px-4">
+              <a href="https://linkedin.com/in/james-rowa" target="blank">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+                  alt="james rowa"
+                  height="20"
+                  width="30"
+                />
+              </a>
+            </span>
+            <span class="px-4">
+              <a href="https://github.com/rowah" target="blank">
+                <img
+                  src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/github.svg"
+                  alt="james rowa"
+                  height="20"
+                  width="30"
+                />
+              </a>
+            </span>
+          </p>
+        </section>
+      </div>
     </Layout>
   );
 };
