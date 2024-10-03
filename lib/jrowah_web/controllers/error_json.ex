66cd868b4bdc24e04d97c8be51c837c281b7d1c9
Rowa <jrowah@gmail.com>
2024-10-03 15:19:59 +0300

    fix dialyzer errors

--- lib/jrowah_web/controllers/error_json.ex
+++ lib/jrowah_web/controllers/error_json.ex
@@ -15,6 +15,7 @@ defmodule JrowahWeb.ErrorJSON do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+  @spec render(String.t(), map()) :: map()
   def render(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
