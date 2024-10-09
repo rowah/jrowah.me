a81ebd03965eac4b3bba5f572042b51831e80540
Rowa <jrowah@gmail.com>
2024-10-09 17:33:01 +0300

    test about page

--- test/jrowah_web/live/about_live_test.exs
+++ test/jrowah_web/live/about_live_test.exs
@@ -3,9 +3,49 @@ defmodule JrowahWeb.AboutLiveTest do
 
   import Phoenix.LiveViewTest
 
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/about")
-    assert disconnected_html =~ "About"
-    assert render(page_live) =~ "About"
+  describe "/about" do
+    test "renders about page", %{conn: conn} do
+      {:ok, page_live, html} = live(conn, "/about")
+
+      assert page_live
+
+      assert html =~
+               "I have experience working with Elixir, Phoenix, Postgres, LiveView, and TailwindCSS, along with UI libraries like Flowbite and TailwindUI."
+    end
+
+    test "clicking the home link takes you to the home page", %{conn: conn} do
+      {:ok, home_live, _html} = live(conn, "/about")
+
+      assert {:error, {:live_redirect, %{kind: :push, to: "/"}}} =
+               home_live
+               |> element(~s|a:fl-contains("Home")|)
+               |> render_click()
+    end
+
+    test "clicking the projects link takes you to the projects page", %{conn: conn} do
+      {:ok, home_live, _html} = live(conn, "/about")
+
+      assert {:error, {:live_redirect, %{kind: :push, to: "/projects"}}} =
+               home_live
+               |> element(~s|a:fl-contains("Projects")|)
+               |> render_click()
+    end
+
+    test "clicking the blog link takes you to the blog page", %{conn: conn} do
+      {:ok, home_live, _html} = live(conn, "/about")
+
+      assert {:error, {:live_redirect, %{kind: :push, to: "/blog"}}} =
+               home_live
+               |> element(~s|a:fl-contains("Blog")|)
+               |> render_click()
+
+      # TODO: Test the newly rendered liveview
+    end
+
+    test "disconnected and connected render", %{conn: conn} do
+      {:ok, page_live, disconnected_html} = live(conn, "/about")
+      assert disconnected_html =~ "About"
+      assert render(page_live) =~ "About"
+    end
   end
 end
